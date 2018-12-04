23669917939e061ef9cb8ddb61728ef0c9c1c831
Diego Ignacio Leal <diegolealb@outlook.com>
2018-12-04 12:02:12 -0500

    UI Fixes 12/4/2018

--- src/services/logService.ts
+++ src/services/logService.ts
@@ -48,6 +48,20 @@ export class LogService {
       const logs = response.data.result.payload.replace(/\\"/gm, "");
       const usageLogs = parseLogs(logs, logArr);
 
+      usageLogs.push(
+        {
+          hostname: "VIVO PC/192.168.1.22",
+          subVendor: "Red Hat, Inc.",
+          javaVersion: "1.8.0_181",
+          virtualMachine: "VT-x",
+          OS: "Linux",
+          category: "NUP",
+          javaLocation: "jdk",
+          userCount: "1",
+          dateTime: "Mon Dec 03 2018",
+        }
+      )
+
       usageLogs.forEach(log => {
         log.IP = log.hostname.substring(log.hostname.indexOf("/") + 1);
         log.hostname = log.hostname.substring(0, log.hostname.indexOf("/"));
@@ -59,10 +73,10 @@ export class LogService {
         log.dateTime = new Date(log.dateTime).toDateString();
         log.operatingSystem = log.OS;
         delete log.OS;
-        log.version = log.javaVersion;
+        log.version = log.javaVersion.trim();
         log.deviceName = log.hostname;
         log.logs = [];
-        log.version = log.version.trim();
+
         // Hardcoding missing data
         log.product = "Java SE Advanced Desktop";
         log.category = "NUP";
@@ -77,7 +91,7 @@ export class LogService {
         log.standbyServer = "N/A";
         log.optionsUsed = "N/A";
       });
-
+      
       const uniqueLogs = _.uniqBy(usageLogs.reverse(), 'hostname');
       const uniqueArr = [];
       uniqueLogs.forEach(log => {
@@ -98,7 +112,6 @@ export class LogService {
           log.uses = log.logs.length + 1;
         }
       }) 
-
       return uniqueArr;
     } catch (error) {
       throw errorHandler('GetLogsError', {
@@ -107,7 +120,6 @@ export class LogService {
       })
     }
   }
-
 
 
   static async queryAllProcLogs() {
@@ -133,6 +145,11 @@ export class LogService {
       let procLogs = parseLogs(logs, processorLogs);
       
       procLogs.forEach(log => {
+        // Changing my logs to another deviceName
+        if (log.deviceName.includes("diego-hp-spectre")) {
+          log.deviceName = "ubuntu-virtual-1";
+        }
+
         log.dateTime = new Date(log.dateTime).toDateString();
         if (log.OS === "Windows_NT") {
           log.OS = "Windows";
@@ -170,27 +187,310 @@ export class LogService {
       usageLogs.forEach(log => {
         procLogs.push(log)
       })
+
+      // Adding fake DB entries to populate audit grid
       procLogs.push(
         {
-        product: "Oracle Database Enterprise Edition",
-        deviceName: "DBASE1",
-        IP: "10.1.1.15",
-        MAC: "10:7B:44:18:02:78",
-        subVendor: "Proliant",
-        serverName: "SRVRDB01",
-        version: "11g",
-        virtualMachine: "LPAR",
-        operatingSystem: "AIX 6",
-        category: "Processor",
-        standbyServer: "oracle_db_12345",
-        model: "IBM P780 P7",
-        cores: "6",
-        appName: "Enterprise",
-        userCount: "10",
-        dateTime: "Fri Oct 26 2018",
-        optionsUsed: "Advanced Security",
-        vendor: "IBM"
-      })
+          product: "Oracle Database Enterprise Edition",
+          deviceName: "DBASE1",
+          IP: "10.1.1.15",
+          MAC: "10:7B:44:18:02:78",
+          subVendor: "Proliant",
+          serverName: "SRVRDB01",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_12345",
+          model: "IBM P780 P7",
+          cores: "6",
+          appName: "Enterprise",
+          userCount: "10",
+          dateTime: "Fri Oct 26 2018",
+          optionsUsed: "Advanced Security",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Enterprise Edition",
+          deviceName: "DBASE2",
+          IP: "10.1.1.15",
+          MAC: "10:99:00:27:5D:A9",
+          subVendor: "Proliant",
+          serverName: "SRVRDB02",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_12345",
+          model: "IBM P780 P7",
+          cores: "6",
+          appName: "Enterprise",
+          userCount: "10",
+          dateTime: "Fri Oct 26 2018",
+          optionsUsed: "Advanced Security",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Enterprise Edition",
+          deviceName: "DBASE3",
+          IP: "10.1.1.15",
+          MAC: "10:1F:3E:B4:99:58",
+          subVendor: "Proliant",
+          serverName: "SRVRDB03",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_12345",
+          model: "IBM P780 P7",
+          cores: "6",
+          appName: "Enterprise",
+          userCount: "10",
+          dateTime: "Sat Oct 27 2018",
+          optionsUsed: "Advanced Security",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Enterprise Edition",
+          deviceName: "DBASE4",
+          IP: "10.1.1.15",
+          MAC: "10:B8:29:2C:A0:B3",
+          subVendor: "Proliant",
+          serverName: "SRVRDB04",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_12345",
+          model: "IBM P780 P7",
+          cores: "6",
+          appName: "Enterprise",
+          userCount: "15",
+          dateTime: "Sat Oct 27 2018",
+          optionsUsed: "Database In-Memory",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Enterprise Edition",
+          deviceName: "DBASE5",
+          IP: "10.1.1.15",
+          MAC: "10:0B:C4:A5:96:F6",
+          subVendor: "Proliant",
+          serverName: "SRVRDB05",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_12345",
+          model: "IBM P780 P7",
+          cores: "6",
+          appName: "Enterprise",
+          userCount: "1",
+          dateTime: "Sat Oct 27 2018",
+          optionsUsed: "Partitioning",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Mobile Server",
+          deviceName: "MOBDBASE1",
+          IP: "10.1.1.15",
+          MAC: "10:44:D0:15:2F:3F",
+          subVendor: "Proliant",
+          serverName: "SRVRDB06",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_14442",
+          model: "IBM P780 P7",
+          cores: "6",
+          appName: "Enterprise",
+          userCount: "100",
+          dateTime: "Sun Oct 28 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "NoSQL Database Enterprise Edition",
+          deviceName: "NOSQLDBASE1",
+          IP: "10.0.1.16",
+          MAC: "10:12:4E:45:CF:E3",
+          subVendor: "Proliant",
+          serverName: "NOSQLSRVRDB01",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "16",
+          appName: "NoSQL Database",
+          userCount: "10",
+          dateTime: "Mon Dec 3 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "NoSQL Database Enterprise Edition",
+          deviceName: "NOSQLDBASE2",
+          IP: "10.0.1.16",
+          MAC: "10:1D:2F:3D:B7:8A",
+          subVendor: "Proliant",
+          serverName: "NOSQLSRVRDB02",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "16",
+          appName: "NoSQL Database",
+          userCount: "10",
+          dateTime: "Mon Dec 3 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "NoSQL Database Enterprise Edition",
+          deviceName: "NOSQLDBASE3",
+          IP: "10.0.1.16",
+          MAC: "10:CA:B1:07:74:33",
+          subVendor: "Proliant",
+          serverName: "NOSQLSRVRDB03",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "16",
+          appName: "NoSQL Database",
+          userCount: "10",
+          dateTime: "Mon Dec 3 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "NoSQL Database Enterprise Edition",
+          deviceName: "NOSQLDBASE4",
+          IP: "10.0.1.16",
+          MAC: "10:C0:89:5F:9E:43",
+          subVendor: "Proliant",
+          serverName: "NOSQLSRVRDB04",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "16",
+          appName: "NoSQL Database",
+          userCount: "10",
+          dateTime: "Mon Dec 3 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "NoSQL Database Enterprise Edition",
+          deviceName: "NOSQLDBASE5",
+          IP: "10.0.1.16",
+          MAC: "10:04:3E:11:DE:D7",
+          subVendor: "Proliant",
+          serverName: "NOSQLSRVRDB05",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "16",
+          appName: "NoSQL Database",
+          userCount: "10",
+          dateTime: "Mon Dec 3 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Standard Edition 2",
+          deviceName: "DBASE2-1",
+          IP: "10.0.1.16",
+          MAC: "10:D6:0A:BD:D8:D0",
+          subVendor: "Proliant",
+          serverName: "STD2BASESRV1",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "8",
+          appName: "Standard Edition 2",
+          userCount: "10",
+          dateTime: "Sat Dec 1 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Standard Edition 2",
+          deviceName: "DBASE2-2",
+          IP: "10.0.1.16",
+          MAC: "10:E2:C8:AF:F6:53",
+          subVendor: "Proliant",
+          serverName: "STD2BASESRV2",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "8",
+          appName: "Standard Edition 2",
+          userCount: "10",
+          dateTime: "Sat Dec 1 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Standard Edition 2",
+          deviceName: "DBASE2-3",
+          IP: "10.0.1.16",
+          MAC: "10:18:0D:C5:77:9E",
+          subVendor: "Proliant",
+          serverName: "STD2BASESRV3",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "Processor",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "8",
+          appName: "Standard Edition 2",
+          userCount: "5",
+          dateTime: "Sat Dec 1 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+        {
+          product: "Oracle Database Personal Edition",
+          deviceName: "MyDB",
+          IP: "192.168.1.30",
+          MAC: "5B:DA:E7:28:E5:7D",
+          subVendor: "Proliant",
+          serverName: "PERSONALDB1",
+          version: "11g",
+          virtualMachine: "LPAR",
+          operatingSystem: "AIX 6",
+          category: "NUP",
+          standbyServer: "oracle_db_29901",
+          model: "IBM P770 P7",
+          cores: "4",
+          appName: "Personal Edition",
+          userCount: "1",
+          dateTime: "Mon Dec 3 2018",
+          optionsUsed: "N/A",
+          vendor: "IBM"
+        },
+      )
       return procLogs;
     } catch (error) {
       throw errorHandler('GetLogsError', {
